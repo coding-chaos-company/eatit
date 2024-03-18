becb5f4c0a965fe8e2acf6727f7cd4c42de2192f
Daiki Maekawa <k1tikurisu@gmail.com>
2024-03-18 13:41:49 +0900

    fix

--- frontend/src/contents/index.tsx
+++ frontend/src/contents/index.tsx
@@ -77,7 +77,7 @@ const Index = () => {
 
   return (
     <Container>
-      {dinoStatus ? (
+      {dinoStatus && dinoStatus.level > 0 ? (
         <DinoHome
           isMe={isMe}
           dinoStatus={dinoStatus}
