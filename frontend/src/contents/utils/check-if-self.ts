bc5cd10336418ceffcd259ecbc9ce1875fd0353e
Daiki Maekawa <k1tikurisu@gmail.com>
2024-03-18 14:28:48 +0900

    自分のユーザページかどうかの判定修正

--- frontend/src/contents/utils/check-if-self.ts
+++ frontend/src/contents/utils/check-if-self.ts
@@ -1,8 +1,6 @@
 export const checkIfSelf = () => {
   // アクセス時のContribution Settingsの有無でログインユーザかどうか判定する
-  const contributionSettings = document.querySelector(
-    '#user-profile-frame > div > div:nth-child(3) > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1) > details'
-  );
+  const contributionSettings = document.querySelector('summary.pinned-items-setting-link');
 
   return !!contributionSettings;
 };
