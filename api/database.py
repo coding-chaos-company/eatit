9cf885387f20dc252a79c0ca3bbe26e1a076bdac
hyphen-o <otekapu@gmail.com>
2024-03-19 12:48:28 +0900

    微修正

--- api/database.py
+++ api/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ASYNC_DB_URL = "sqlite+aiosqlite:///db.sqlite3"
+ASYNC_DB_URL = "sqlite+aiosqlite:///eatit.sqlite3"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
