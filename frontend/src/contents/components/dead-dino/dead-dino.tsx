7ce8cabad45e6247640a5fc9c01bd05206763a13
Daiki Maekawa <k1tikurisu@gmail.com>
2024-03-20 03:03:04 +0900

    遺影の処理

--- frontend/src/contents/components/dead-dino/dead-dino.tsx
+++ frontend/src/contents/components/dead-dino/dead-dino.tsx
@@ -1,10 +1,18 @@
 import deathBrachioImage from 'data-base64:~/../assets/death-brachio.png';
+import type { MouseEventHandler } from 'react';
 import * as styles from './dead-dino.module.css';
 
-export const DeadDino = () => {
+type DeadDinoProps = {
+  onClick: MouseEventHandler;
+};
+
+export const DeadDino = ({ onClick }: DeadDinoProps) => {
   return (
     <div className={styles.deadDino}>
       <img className={styles.img} src={deathBrachioImage} alt="dead dino" />
+      <button className={styles.button} type="button" onClick={onClick}>
+        {chrome.i18n.getMessage('deadDino_reStartButton')}
+      </button>
     </div>
   );
 };
