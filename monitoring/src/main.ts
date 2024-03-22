2a4f46b3fee1a97537b541abca5fa2dfe79c0bcf
Daiki Maekawa <k1tikurisu@gmail.com>
2024-03-22 23:55:20 +0900

    fix url

--- monitoring/src/main.ts
+++ monitoring/src/main.ts
@@ -1,6 +1,6 @@
 global.main = main;
 
 function main() {
-  const url = "https://eatit-4xol.onrender.com";
+  const url = "https://eatit-7esa.onrender.com/";
   UrlFetchApp.fetch(url).getContentText();
 }
