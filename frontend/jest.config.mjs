c3d73a8061f959a50f52fee5ac92012d2f775e38
Daiki Maekawa <k1tikurisu@gmail.com>
2024-03-30 00:24:50 +0900

    add unit test setup file

--- frontend/jest.config.mjs
+++ frontend/jest.config.mjs
@@ -10,6 +10,7 @@ const config = {
   moduleDirectories: ['node_modules', '<rootDir>/src/', '<rootDir>/tests/unit/'],
   moduleNameMapper: { '@/(.*)$': '<rootDir>/src/$1', 'data-base64:~/../(.*)$': '<rootDir>/$1' },
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/utils/jest-setup.ts'],
   transform: {
     '^.+\\.ts?$': ['ts-jest', { isolatedModules: true, useESM: true }],
     '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig: { jsx: 'react-jsx' } }],
